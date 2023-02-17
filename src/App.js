1e8fc49fa2e709003aa80de7b416bd50e98f5440
Adnan Niaz <sanixstudio@gmail.com>
2023-02-17 09:52:50 -0800

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/system";
+import "./App.css";
+import ContainedButtons from "./components/buttons/ContainedButtons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <h1>Hello</h1>
+      <ContainedButtons />
+    </Box>
   );
 }
 
